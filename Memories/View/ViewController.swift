4da3f273b58ebc70ef7666f6af343e0d86a0f2be
Naomi Schettini <naomischettini@gmail.com>
2021-05-30 11:36:00 -0400

    chore: laid out basic skeleton architecture for photo list app

---
+++ Memories/View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  memories
+//
+//  Created by Naomi Schettini on 5/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
