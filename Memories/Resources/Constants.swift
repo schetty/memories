ab1fb46ba24906877361a2dba93a75e6b531de28
Naomi Schettini <naomischettini@gmail.com>
2021-05-31 01:10:00 -0400

    chore: added colors and default image asset

---
+++ Memories/Resources/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  Memories
+//
+//  Created by Naomi Schettini on 5/30/21.
+//
+
+import UIKit
+
+struct GlobalConstants {
+    
+    struct Colors {
+        
+        struct AlbumListGradient {
+            static let tumbleweed = UIColor(red: (210.0/255.0), green: (272.0/255.0), blue: (253.0/255.0), alpha: 1.0)
+            static let ash = UIColor(red: (189.0/255.0), green: (190.0/255.0), blue: (169.0/255.0), alpha: 1.0)
+            static let seaGreen = UIColor(red: (141.0/255.0), green: (179.0/255.0), blue: (139.0/255.0), alpha: 1.0)
+            static let viridian = UIColor(red: (86.0/255.0), green: (135.0/255.0), blue: (109.0/255.0), alpha: 1.0)
+            static let springGreen = UIColor(red: (4.0/255.0), green: (114.0/255.0), blue: (77.0/255.0), alpha: 1.0)
+        }
+    }
+}
